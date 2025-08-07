2eec32a39c9e4b6254c18c16a3e22cf6b37b0420
Emerson Junior <emerson.c.junior.115@gmail.com>
2025-08-07 14:23:12 -0300

    fix: corrigindo erro de deploy

--- src/app/game/[id]/page.tsx
+++ src/app/game/[id]/page.tsx
@@ -1,40 +1,50 @@
-import { redirect } from "next/navigation";
 import Image from "next/image";
+import { redirect } from "next/navigation";
+import Container from "@/components/Container";
+import Label from "./components/Label";
 import GameCard from "@/components/GameCard";
+import { IGame } from "@/interfaces/Game";
 
-const getGameData = async (id: string) => {
-  const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game&id=${id}`, {
-    cache: "no-store",
-  });
+async function getGameData(id: string): Promise<IGame | null> {
+  try {
+    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game&id=${id}`, { cache: "no-store" });
+    if (!res.ok) return null;
+    return await res.json();
+  } catch {
+    return null;
+  }
+}
 
-  if (!res.ok) return null;
-  return res.json();
-};
+async function getRandomGame(): Promise<IGame | null> {
+  try {
+    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`, { cache: "no-store" });
+    if (!res.ok) return null;
+    return await res.json();
+  } catch {
+    return null;
+  }
+}
 
-const getRandomGame = async () => {
-  const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=random`, {
-    cache: "no-store",
-  });
-
-  if (!res.ok) return null;
-  return res.json();
-};
-
-export async function generateMetadata({ params }: Props) {
+export async function generateMetadata({ params }: { params: { id: string } }) {
   const game = await getGameData(params.id);
 
   if (!game) {
     return {
-      title: "Jogo não encontrado",
+      title: "DalyGames - Descubra jogos incríveis para se divertir.",
     };
   }
 
   return {
-    title: `${game.name} - DalyGames`,
+    title: game.title,
+    description: game.description.slice(0, 100),
+    openGraph: {
+      title: game.title,
+      images: [game.image_url],
+    },
   };
 }
 
-export default async function GameDetails({ params }: Props) {
+export default async function GameDetails({ params }: { params: { id: string } }) {
   const game = await getGameData(params.id);
 
   if (!game) {
@@ -44,36 +54,48 @@ export default async function GameDetails({ params }: Props) {
   const randomGame = await getRandomGame();
 
   return (
-    <div className="w-full flex flex-col gap-10">
-      <div className="relative w-full h-[450px] rounded-lg overflow-hidden">
+    <main>
+      <div className="bg-black h-80 sm:h-96 w-full relative">
         <Image
+          className="object-cover w-full h-80 sm:h-96 opacity-80"
           src={game.image_url}
-          alt={`Imagem do jogo ${game.name}`}
-          fill
-          className="object-cover"
-          quality={100}
+          alt={game.title}
           priority
-          sizes="100vw"
+          fill
+          quality={100}
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 33vw"
         />
       </div>
 
-      <div>
-        <h1 className="text-3xl font-bold">{game.name}</h1>
-        <p className="text-zinc-400 text-base">{game.description}</p>
-      </div>
+      <Container>
+        <h1 className="font-bold text-xl my-4">{game.title}</h1>
+        <p>{game.description}</p>
 
-      <div>
-        <h2 className="text-2xl font-semibold mb-4">Veja também</h2>
-        <div className="w-full max-w-[260px]">
-          <GameCard game={randomGame} />
+        <h2 className="font-bold text-lg mt-7 mb-2">Plataformas</h2>
+        <div className="flex gap-3 flex-wrap">
+          {game.platforms.map((platform) => (
+            <Label key={platform} name={platform} />
+          ))}
         </div>
-      </div>
-    </div>
+
+        <h2 className="font-bold text-lg mt-7 mb-2">Categorias</h2>
+        <div className="flex gap-3 flex-wrap">
+          {game.categories.map((category) => (
+            <Label key={category} name={category} />
+          ))}
+        </div>
+
+        <p className="mt-7 mb-2">
+          <span className="font-semibold">Data de lançamento: </span> {game.release}
+        </p>
+
+        <h2 className="font-bold text-lg mt-7 mb-2">Jogo recomendado:</h2>
+        <div className="flex">
+          <div className="flex-1">
+            <GameCard game={randomGame!} />
+          </div>
+        </div>
+      </Container>
+    </main>
   );
 }
-
-type Props = {
-  params: {
-    id: string;
-  };
-};
